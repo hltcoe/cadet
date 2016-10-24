3b554180d465f2e8dbe129be235d2c6d61cda021
Craig Harman <craig@craigharman.net>
2016-10-24 15:35:03 -0400

    Added logging/validation to SearchProxyHandler from (deleted) SearchHandler class

--- cadet-ui/src/main/java/edu/jhu/hlt/cadet/search/SearchProxyHandler.java
+++ cadet-ui/src/main/java/edu/jhu/hlt/cadet/search/SearchProxyHandler.java
@@ -2,6 +2,7 @@ package edu.jhu.hlt.cadet.search;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import edu.jhu.hlt.concrete.search.SearchCapability;
 import edu.jhu.hlt.concrete.search.SearchProxyService;
 import edu.jhu.hlt.concrete.search.SearchQuery;
 import edu.jhu.hlt.concrete.search.SearchResult;
+import edu.jhu.hlt.concrete.search.SearchResultItem;
 import edu.jhu.hlt.concrete.services.ServiceInfo;
 import edu.jhu.hlt.concrete.services.ServicesException;
 
@@ -66,12 +68,62 @@ public class SearchProxyHandler implements SearchProxyService.Iface {
     public SearchResult search(SearchQuery query, String providerName)
             throws ServicesException, TException {
         SearchProvider searchProvider = providerMap.get(providerName);
-        if (searchProvider != null) {
-            logger.info("search() called for SearchProvider " + providerName);
-            return searchProvider.search(query);
-        }
-        else {
+        if (searchProvider == null) {
             throw new ServicesException("Unable to find configuration for SearchProvider named " + providerName);
+        }
+
+        logSearchQuery(query);
+
+        validate(query);
+
+        SearchResult result = searchProvider.search(query);
+
+        logSearchResults(result);
+
+        return result;
+    }
+
+    private void validate(SearchQuery query) throws ServicesException {
+        if (query.getRawQuery() == null || query.getRawQuery().isEmpty()) {
+            throw new ServicesException("Search query is empty");
+        }
+    }
+
+    protected static void logSearchQuery(SearchQuery searchQuery) {
+        logger.info("Search query: " + searchQuery.getRawQuery());
+
+        Iterator<String> questionIterator = searchQuery.getQuestionsIterator();
+        Iterator<String> termIterator = searchQuery.getTermsIterator();
+
+        if (searchQuery.getTermsSize() == 0) {
+            logger.debug("No Terms provided");
+        } else {
+            logger.debug(searchQuery.getTermsSize() + " Term(s) provided");
+            while (termIterator.hasNext()) {
+                logger.debug("Term: " + termIterator.next());
+            }
+        }
+
+        if (searchQuery.getQuestionsSize() == 0) {
+            logger.debug("No questions provided");
+        } else {
+            logger.debug(searchQuery.getQuestionsSize() + " question(s) provided");
+            while (questionIterator.hasNext()) {
+                logger.debug("Question: " + questionIterator.next());
+            }
+        }
+    }
+
+    protected static void logSearchResults(SearchResult searchResults) {
+        Iterator<SearchResultItem> searchResultsIterator = searchResults.getSearchResultItemsIterator();
+
+        if (searchResults.getSearchResultItemsSize() == 0) {
+            logger.info("Search: No results returned");
+        } else {
+            logger.info("Search: " + searchResults.getSearchResultItemsSize() + " result(s) provided");
+            while (searchResultsIterator.hasNext()) {
+                logger.debug("SearchResult: " + searchResultsIterator.next());
+            }
         }
     }
 }
