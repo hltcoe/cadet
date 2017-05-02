4637000d08dbb7fc82c508d6430338527bbd9375
Adam Poliak <azpoliak@gmail.com>
2017-05-02 14:25:09 -0400

    WIP: UI: got the React components to load before loading the text. Causes a console error saying jquery is needed

--- cadet-ui/src/main/webapp/js/sentence_event_cadet.js
+++ cadet-ui/src/main/webapp/js/sentence_event_cadet.js
@@ -65,131 +65,6 @@ function updateDisplayedCommunications(comms, first) {
 var COMMS = [];
 var RESULTS_SERVER_SESSION_ID = null;
 
-$(document).ready(function(){
-   //$('#menu1 .businessSpecifics').remove();
-    //$('#menu1 .businessSpecifics').remove();
-    //var number=$("#selectStores option:selected").text();
-     /*$(".add_event").click(function() {
-       addButton(this);
-     });
-
-     $(".next_sentence").click(function() {
-        $(".events")
-        var url = window.location.href;
-        var data = "test data";
-        $.post(url+"next", data,
-          function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-          }, "JSON"
-        );
-        //alert("Next sentence to annotate");
-      });
-
-      var mouseIsIn;
-      $('.outer').hover(function() {
-         mouseIsIn = true;
-      }, function() {
-         mouseIsIn = false;
-      });
-
-      $('.outer').blur(function(){
-         if (!mouseIsIn) {
-             $('.outer').removeClass('on');
-             $('.outer').addClass('off');
-             $( '.outer' ).removeClass( "open" );
-         }
-      });
-
-     function addButton(ele) {
-        //alert("Add event!")
-        if (ele.parentElement.childElementCount > 13) {
-          ele.remove()
-          return
-        }
-        /*ReactDOM.render(
-          <EventTag />,
-          //React.createElement(new EventTag, null),
-          document.getElementById('content-events')
-        );*/
-        /*
-        lastNode = ele.parentElement.children[ele.parentElement.childElementCount-2];
-        clone = lastNode.cloneNode(true).childNodes[0];
-        labels = clone.childNodes[1];
-        for (i = 0; i < labels.length; i++) {
-          labels.children[i].firstElementChild.name += "_event_"+ele.parentElement.childElementCount -1;
-          labels.children[i].firstElementChild.checked = true;
-        }
-        checkedLabel = Math.round(Math.random() * 5);
-        console.log(checkedLabel)
-        labels.children[checkedLabel].firstElementChild.checked = true;
-        ele.parentElement.insertBefore(clone, ele);*/
-     //};
-
-     /*$.getJSON('uz_data/uz_'+1+'_.concrete.json', function(commJSONData) {
-
-       var comm = new Communication();
-       comm.initFromTJSONProtocolObject(commJSONData);
-
-       var tokenizationList = comm.getTokenizationsAsList();
-       while (tokenizationList.length < 500) {
-         var tok = Token;
-         tok.text = "\t ";
-         tokenizationList.push(tok);
-       }
-       var tokenization = tokenizationList[4];//[1];
-
-       var tokenizationWidget = $('#tokenization').tokenizationWidget(
-         tokenization, {whitespaceTokenization: true});
-
-     });*/
-    //$.getScript('../cadet.js', function() {
-     CADET.init();
-     var searchResultIdString = getUrlParameter('searchResultId');
-     if (searchResultIdString) {
-         var searchResultId = new UUID();
-         searchResultId.uuidString = searchResultIdString;
-
-         try {
-             RESULTS_SERVER_SESSION_ID = CADET.results.startSession(searchResultId);
-             var annotationUnitIdentifiers = CADET.results.getNextChunk(RESULTS_SERVER_SESSION_ID);
-             COMMS = getNextCommunications(annotationUnitIdentifiers);
-         }
-         catch (error) {
-             // TODO: Error handling
-             throw error;
-         }
-     }
-     else {
-         // TODO: User-friendly error message about missing searchResultId
-     }
-     $('#save_button').on('click', function(event) {
-         try {
-             if (COMMS && COMMS.length > 0) {
-                 for (var i = 0; i < COMMS.length; i++) {
-                     CADET.results.submitAnnotation(
-                         RESULTS_SERVER_SESSION_ID,
-                         // The .annotationUnitIdentifier field is added by getNextCommunications()
-                         COMMS[i].annotationUnitIdentifier,
-                         COMMS[i]);
-                 }
-             }
-             var annotationUnitIdentifiers = CADET.results.getNextChunk(RESULTS_SERVER_SESSION_ID);
-             COMMS = getNextCommunications(annotationUnitIdentifiers);
-             if (COMMS.length > 0) {
-                 updateDisplayedCommunications(COMMS, false);
-             }
-             else {
-                 location.replace("results.html");
-             }
-         }
-         catch (error) {
-         }
-     });
-
-     updateDisplayedCommunications(COMMS, true);
-    //});
-});
-
 var funcs = [];
 
 var events = {}
@@ -389,3 +264,127 @@ ReactDOM.render(
   <SubmitButton />,
   document.getElementById('submit-events')
 );*/
+$(document).ready(function(){
+   //$('#menu1 .businessSpecifics').remove();
+    //$('#menu1 .businessSpecifics').remove();
+    //var number=$("#selectStores option:selected").text();
+     /*$(".add_event").click(function() {
+       addButton(this);
+     });
+
+     $(".next_sentence").click(function() {
+        $(".events")
+        var url = window.location.href;
+        var data = "test data";
+        $.post(url+"next", data,
+          function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+          }, "JSON"
+        );
+        //alert("Next sentence to annotate");
+      });
+
+      var mouseIsIn;
+      $('.outer').hover(function() {
+         mouseIsIn = true;
+      }, function() {
+         mouseIsIn = false;
+      });
+
+      $('.outer').blur(function(){
+         if (!mouseIsIn) {
+             $('.outer').removeClass('on');
+             $('.outer').addClass('off');
+             $( '.outer' ).removeClass( "open" );
+         }
+      });
+
+     function addButton(ele) {
+        //alert("Add event!")
+        if (ele.parentElement.childElementCount > 13) {
+          ele.remove()
+          return
+        }
+        /*ReactDOM.render(
+          <EventTag />,
+          //React.createElement(new EventTag, null),
+          document.getElementById('content-events')
+        );*/
+        /*
+        lastNode = ele.parentElement.children[ele.parentElement.childElementCount-2];
+        clone = lastNode.cloneNode(true).childNodes[0];
+        labels = clone.childNodes[1];
+        for (i = 0; i < labels.length; i++) {
+          labels.children[i].firstElementChild.name += "_event_"+ele.parentElement.childElementCount -1;
+          labels.children[i].firstElementChild.checked = true;
+        }
+        checkedLabel = Math.round(Math.random() * 5);
+        console.log(checkedLabel)
+        labels.children[checkedLabel].firstElementChild.checked = true;
+        ele.parentElement.insertBefore(clone, ele);*/
+     //};
+
+     /*$.getJSON('uz_data/uz_'+1+'_.concrete.json', function(commJSONData) {
+
+       var comm = new Communication();
+       comm.initFromTJSONProtocolObject(commJSONData);
+
+       var tokenizationList = comm.getTokenizationsAsList();
+       while (tokenizationList.length < 500) {
+         var tok = Token;
+         tok.text = "\t ";
+         tokenizationList.push(tok);
+       }
+       var tokenization = tokenizationList[4];//[1];
+
+       var tokenizationWidget = $('#tokenization').tokenizationWidget(
+         tokenization, {whitespaceTokenization: true});
+
+     });*/
+    //$.getScript('../cadet.js', function() {
+     CADET.init();
+     var searchResultIdString = getUrlParameter('searchResultId');
+     if (searchResultIdString) {
+         var searchResultId = new UUID();
+         searchResultId.uuidString = searchResultIdString;
+
+         try {
+             RESULTS_SERVER_SESSION_ID = CADET.results.startSession(searchResultId);
+             var annotationUnitIdentifiers = CADET.results.getNextChunk(RESULTS_SERVER_SESSION_ID);
+             COMMS = getNextCommunications(annotationUnitIdentifiers);
+         }
+         catch (error) {
+             // TODO: Error handling
+             throw error;
+         }
+     }
+     else {
+         // TODO: User-friendly error message about missing searchResultId
+     }
+     $('#save_button').on('click', function(event) {
+         try {
+             if (COMMS && COMMS.length > 0) {
+                 for (var i = 0; i < COMMS.length; i++) {
+                     CADET.results.submitAnnotation(
+                         RESULTS_SERVER_SESSION_ID,
+                         // The .annotationUnitIdentifier field is added by getNextCommunications()
+                         COMMS[i].annotationUnitIdentifier,
+                         COMMS[i]);
+                 }
+             }
+             var annotationUnitIdentifiers = CADET.results.getNextChunk(RESULTS_SERVER_SESSION_ID);
+             COMMS = getNextCommunications(annotationUnitIdentifiers);
+             if (COMMS.length > 0) {
+                 updateDisplayedCommunications(COMMS, false);
+             }
+             else {
+                 location.replace("results.html");
+             }
+         }
+         catch (error) {
+         }
+     });
+
+     updateDisplayedCommunications(COMMS, true);
+    //});
+});
