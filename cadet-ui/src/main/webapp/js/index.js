54544d833699b2dd41aebaeaee4371172c9a71f8
Adam Poliak <azpoliak@gmail.com>
2017-10-26 16:11:28 -0400

    issue-46. created a map to store communications when calling fetch based upon search results

--- cadet-ui/src/main/webapp/js/index.js
+++ cadet-ui/src/main/webapp/js/index.js
@@ -4,6 +4,7 @@
 // Global variables
 var SEARCH_RESULT_TABLE;
 var SEARCH_RESULT;
+var COMMS_MAP;
 
 function createResultsTable() {
     SEARCH_RESULT_TABLE = $('#results_table').DataTable({
@@ -71,14 +72,20 @@ function createResultsTable() {
                         // the Communication when the render type is 'display'
                         //   https://datatables.net/reference/option/columns.render
                         try {
-                            var fetchResult = CADET.fetchComms([searchResultItem.communicationId]);
-                            if (fetchResult.communications.length === 1) {
-                                searchResultItem.communication = fetchResult.communications[0];
-                                searchResultItem.communication.addInternalReferences();
+                            if (searchResultItem.communicationId in COMMS_MAP) {
+                                var comm = COMMS_MAP[searchResultItem.communicationId];
+                                searchResultItem.sentence = comm.getSentenceWithUUID(searchResultItem.sentenceId);
+                            } else {
+                              var fetchResult = CADET.fetchComms([searchResultItem.communicationId]);
+                              if (fetchResult.communications.length === 1) {
+                                  searchResultItem.communication = fetchResult.communications[0];
+                                  searchResultItem.communication.addInternalReferences();
 
-                                // searchResultItem.sentence will be null if searchResultItem.sentenceId is not valid
-                                searchResultItem.sentence = searchResultItem.communication.getSentenceWithUUID(
-                                    searchResultItem.sentenceId);
+                                  // searchResultItem.sentence will be null if searchResultItem.sentenceId is not valid
+                                  searchResultItem.sentence = searchResultItem.communication.getSentenceWithUUID(
+                                      searchResultItem.sentenceId);
+                                  COMMS_MAP[searchResultItem.communicationId] = searchResultItem.communication;
+                              }
                             }
                         }
                         catch (error) {
@@ -303,10 +310,12 @@ function executeSearchQuery(searchQuery) {
     catch (error) {
         displayAndThrowError(error);
     }
-
     addResultToResultsTable(searchResult);
 
     $('a[href="#search_results"]').tab('show');
+    if (Object.keys(COMMS_MAP) > 35) {
+      COMMS_MAP = {};
+    }
 }
 
 function executeSearchQueryEventHandler(event) {
@@ -466,7 +475,7 @@ $(document).ready(function() {
             executeSearchQueryFromSearchBox();
         }
     });
-
+    COMMS_MAP = {};
     createResultsTable();
 
     $('#search_button').on('click', executeSearchQueryFromSearchBox);
